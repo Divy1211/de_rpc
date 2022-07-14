bdaedefeaf278b03060cf1841fbf882334b08eeb
Alian713 <divy1211.dc@gmail.com>
2022-07-15 06:27:17 +0800

    cleaned up unused invite code

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import {getLobby, getLobbyDetails} from "./find_lobby";
 import {getMatch, getMatchDetails, loadStrings} from "./find_match";
 import {getSteamId} from "./steam_id";
 import {ensure} from "./utils/general";
-import open from "open";
 
 dotenv.config();
 
@@ -16,9 +15,9 @@ const client = new RPC.Client({transport: "ipc"});
 RPC.register(ensure(process.env.ID));
 
 async function setActivity() {
-    // const test = await exec("tasklist");
-    // if (!test.stdout.includes(`AoE2DE_s.exe`))
-    //     return;
+    const test = await exec("tasklist");
+    if (!test.stdout.includes(`AoE2DE_s.exe`))
+        return;
 
     const steamId = await getSteamId();
 
@@ -46,11 +45,6 @@ async function setActivity() {
             details: "In the main menu or",
             state: "maybe in the ranked queue",
             instance: false,
-            partyId: "123456789",
-            partySize: 1,
-            partyMax: 8,
-            joinSecret: "aoe2de://0/123456789",
-            spectateSecret: "aoe2de://1/123456789",
         });
         return;
     }
@@ -74,17 +68,11 @@ async function setActivity() {
     });
 }
 
-// open("aoe2de://0/12345789", {wait: true}).then();
-
 client.on("ready", async () => {
     await loadStrings();
     await setActivity();
     console.log("Starting Loop");
     setInterval(setActivity, 15 * 1000);
-});
-
-client.on("joinGame", async (partyId, partySize, partyMax, joinSecret) => {
-    console.log(partyId, partySize, partyMax, joinSecret);
 });
 
 client.login({clientId: ensure(process.env.ID)}).then();
